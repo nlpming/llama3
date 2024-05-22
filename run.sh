4341883e49ff6434dddfbb7e8195344ecde4ec17
nlpming <qqchenzhiming@163.com>
2024-05-22 23:10:33 +0800

    更新run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 torchrun --nproc_per_node 1 example_chat_completion.py \
     --ckpt_dir Meta-Llama-3-8B-Instruct/ \
     --tokenizer_path Meta-Llama-3-8B-Instruct/tokenizer.model \
-    --max_seq_len 512 --max_batch_size 6
+    --max_seq_len 512 --max_batch_size 1
 
